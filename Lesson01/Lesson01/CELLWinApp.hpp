d687726381e617c62cf03479fbe02612dcade99b
gf530142771 <530142771@qq.com>
2016-11-08 10:11:43 +0800

    OpenGL2.0学习-窗口创建
    以面向对象的思想来创建窗口

--- Lesson01/Lesson01/CELLWinApp.hpp
+++ Lesson01/Lesson01/CELLWinApp.hpp
@@ -4,6 +4,37 @@
 class CELLWinApp
 {
 protected:
+	static LRESULT CALLBACK wndProc(HWND hWnd, UINT msg, WPARAM uParam, LPARAM lParam)
+	{
+		// 		switch (msg)
+		// 		{
+		// 		case WM_CREATE:
+		// 			break;
+		// 		case WM_DESTROY:
+		// 			break;
+		// 		case WM_CLOSE:
+		// 			PostQuitMessage(0);
+		// 			break;
+		// 		default:
+		// 			return DefWindowProc(hWnd, msg, uParam, lParam);
+		// 			break;
+		// 		}
+		// 		return S_OK;
+		CELLWinApp* pThis = (CELLWinApp*)GetWindowLong(hWnd, GWL_USERDATA);
+		if (pThis)
+		{
+			pThis->onEvent(hWnd, msg, uParam, lParam);
+		}
+
+		if (WM_CREATE == msg)
+		{
+			CREATESTRUCT* pStruct = (CREATESTRUCT*)lParam;
+			SetWindowLong(hWnd, GWL_USERDATA, (DWORD_PTR)pStruct->lpCreateParams);
+		}
+
+		return DefWindowProc(hWnd, msg, uParam, lParam);
+	}
+protected:
 	HWND _hWnd;
 	HINSTANCE _hInstance;
 public:
@@ -36,16 +67,26 @@ public:
 		UnregisterClass(_T("CELLWinApp"), _hInstance);
 	}
 
-	static LRESULT CALLBACK wndProc(HWND hWnd, UINT msg, WPARAM uParam, LPARAM lParam)
+	virtual LRESULT CALLBACK onEvent(HWND hWnd, UINT msg, WPARAM uParam, LPARAM lParam)
 	{
 		switch (msg)
 		{
-		case WM_CREATE:
+		case WM_CLOSE:
 			break;
 		case WM_DESTROY:
+		{
+						   PostQuitMessage(0);
+		}
 			break;
-		case WM_CLOSE:
-			PostQuitMessage(0);
+		case WM_LBUTTONDOWN:
+			break;
+		case WM_LBUTTONUP:
+			break;
+		case WM_MOUSEMOVE:
+			break;
+		case WM_RBUTTONUP:
+			break;
+		case WM_RBUTTONDOWN:
 			break;
 		default:
 			return DefWindowProc(hWnd, msg, uParam, lParam);
@@ -54,10 +95,10 @@ public:
 		return S_OK;
 	}
 
-	int main(int width ,int height)
+	int main(int width, int height)
 	{
 		//2.创建窗口
-		_hWnd = CreateWindow(_T("CELLWinApp"), _T("CELLWinApp"),WS_OVERLAPPEDWINDOW,0,0,width,height,0,0,_hInstance,this);
+		_hWnd = CreateWindow(_T("CELLWinApp"), _T("CELLWinApp"), WS_OVERLAPPEDWINDOW, 0, 0, width, height, 0, 0, _hInstance, this);
 		if (_hWnd == 0)
 		{
 			return -1;
@@ -70,12 +111,23 @@ public:
 		//消息循环
 		MSG msg = { 0 };
 
-		while (GetMessage(&msg,0,0,0))
+		while (msg.message != WM_QUIT)
 		{
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
+			if (msg.message == WM_CLOSE || msg.message == WM_DESTROY)
+			{
+				break;
+			}
+
+			if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+			{
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+			else {
+				Sleep(1);
+			}
 		}
-		return 1;
+		return 0;
 	}
 };
 
